e5f5952cfbbec0d264bd441adc979ee6c9567f10
Storm <yannic.freson1@gmail.com>
2020-10-23 15:30:24 +0200

    upd

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
-document.addEventListener('click', function (click) {
+/* document.addEventListener('click', function (click) {
   console.log(click.target);
   if (click.target = document.getElementById('home')) {
-    click.target.classList.add('active')
+    click.target.parentElement.classList.add('active')
   }
-})
+}) */
