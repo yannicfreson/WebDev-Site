b89b502307bef87ebe46f2e5206dd4d87027718e
Storm <61149527+yannicfreson@users.noreply.github.com>
2020-11-20 22:24:26 +0100

    guess what, updates!

--- code.js
+++
@@ -1,6 +0,0 @@
-/* document.addEventListener('click', function (click) {
-  console.log(click.target);
-  if (click.target = document.getElementById('home')) {
-    click.target.parentElement.classList.add('active')
-  }
-}) */
